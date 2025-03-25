f19a387965b95602fc86b091a31a230ed6c75360
ayush <ayushatschool5@gmail.com>
2025-03-25 15:48:58 +0530

    feat: setup up lint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-console': ['warn', { allow: ['warn', 'error'] }]
+    },
+    ignores: ['node_modules/', '.next/', 'out/']
+  }
 ];
 
 export default eslintConfig;
