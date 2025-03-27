6811c355ae6e058fbdb4946cf6f2068b8fb32121
ayush <ayushatschool5@gmail.com>
2025-03-27 23:33:30 +0530

    feat: add payment processing functionality with error handling and user feedback

---
+++ src/actions/payments/index.ts
@@ -0,0 +1,44 @@
+'use server';
+import { prisma } from '@/lib/prisma';
+
+type PaymentDetails = {
+  userId: string;
+  amount: number;
+  transactionId: string;
+  merchantTransactionId: string;
+  merchantId: string;
+  status: string;
+};
+
+export const createPaymentRecord = async (details: PaymentDetails) => {
+  try {
+    return await prisma.transaction.create({
+      data: {
+        userId: details.userId,
+        amount: details.amount,
+        transactionId: details.transactionId,
+        merchantTransactionId: details.merchantTransactionId,
+        merchantId: details.merchantId,
+        status: details.status,
+      },
+    });
+  } catch (e) {
+    console.error('Error creating payment record:', e);
+    return null;
+  }
+};
+
+export const getUserPaymentStatus = async (email: string) => {
+  try {
+    return await prisma.transaction.findFirst({
+      where: {
+        user: {
+          email: email,
+        },
+      },
+    });
+  } catch (error) {
+    console.error('Error fetching payment status:', error);
+    return null;
+  }
+};
