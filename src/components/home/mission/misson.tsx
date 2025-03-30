321c8f0e2e50d99cee72afaf498562949f2f6993
ayush <ayushatschool5@gmail.com>
2025-03-30 15:14:23 +0530

    feat: misson section done

--- src/components/home/mission/misson.tsx
+++ src/components/home/mission/misson.tsx
@@ -29,13 +29,14 @@ export default function Mission() {
   return (
     <div className="my-20 flex flex-col items-center justify-center">
       <Title title="Our Mission" />
-      <div className="grid grid-cols-2 w-full max-w-7xl gap-10">
+      <div className="grid grid-cols-1 slg:grid-cols-2 w-full max-w-7xl gap-10">
         {missions.map((mission, index) => (
           <MissionBanner
             key={index}
             title={mission.title}
             description={mission.description}
             imageUrl={mission.imageUrl}
+            direction={index % 2 === 0 ? 'right' : 'left'}
           />
         ))}
       </div>
